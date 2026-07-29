00cc5dec9e3509b41a9b5d6b10fc59c69a7370eb
dspbih <dsp.bih.info@gmail.com>
2026-07-29 15:06:48 +0200

    Update script.js
    Dodan automatski datum i vrijeme

--- css/script.js
+++ css/script.js
@@ -1,1 +1,31 @@
+function prikaziVrijeme() {
 
+    const sada = new Date();
+
+    const dani = [
+        "Nedjelja",
+        "Ponedjeljak",
+        "Utorak",
+        "Srijeda",
+        "Četvrtak",
+        "Petak",
+        "Subota"
+    ];
+
+    const dan = dani[sada.getDay()];
+
+    const datum = sada.toLocaleDateString("bs-BA");
+
+    const vrijeme = sada.toLocaleTimeString("bs-BA", {
+        hour: "2-digit",
+        minute: "2-digit",
+        second: "2-digit"
+    });
+
+    document.getElementById("dan").innerHTML = dan;
+    document.getElementById("datum").innerHTML = datum;
+    document.getElementById("vrijeme").innerHTML = vrijeme;
+}
+
+prikaziVrijeme();
+setInterval(prikaziVrijeme, 1000);
